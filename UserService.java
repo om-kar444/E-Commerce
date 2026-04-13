e5b11d6d918ea4c959325bcdbb367c6e6e9c7eb7
Omkar V Kurane <omkarkurane444@gmail.com>
2026-04-13 20:48:50 +0530

    Delete UserService.java

--- UserService.java
+++
@@ -1,6 +0,0 @@
-package E_CommerceWebsite;
-
-public interface UserService {
-    boolean signup(String username, String password, String se);
-    boolean login(String username, String password);
-}
