4807deef096d19e765f2737e610ae1b67b3d1ee5
Omkar V Kurane <omkarkurane444@gmail.com>
2026-04-16 20:12:38 +0530

    Delete PaymentException.java

--- PaymentException.java
+++
@@ -1,9 +0,0 @@
-package E_CommerceWebsite2;
-
-// Exception Handling: thrown when payment validation fails
-// Inheritance: extends Exception (checked)
-public class PaymentException extends Exception {
-    public PaymentException(String message) {
-        super(message);
-    }
-}
